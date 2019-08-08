a96793343bd11ba13e8ab9f8875bee74e8d8548a
manuelsilv <manuel-silva@live.nl>
2019-08-08 10:18:38 +0200

    auth

--- starter-code/app.js
+++ starter-code/app.js
@@ -48,8 +48,17 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
-
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 const index = require('./routes/index');
 app.use('/', index);
